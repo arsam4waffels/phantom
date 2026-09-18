c0af7801bc132227bf059cc5c09b63c4ca4469c0
Arsam <arsam4waffels@gmail.com>
2026-09-19 00:22:12 +0330

    core: init HttpRequest

---
+++ src/main/java/org/phantom/HttpRequest.java
@@ -0,0 +1,73 @@
+package org.phantom;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private final String method;
+    private final String path;
+    private final String version;
+    private final Map<String, String> headers;
+
+    public HttpRequest(String method,
+                       String path,
+                       String version,
+                       Map<String, String> headers) {
+        this.method     = method;
+        this.path       = path;
+        this.version    = version;
+        this.headers    = headers;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+    public String getPath() {
+        return path;
+    }
+    public String getVersion() {
+        return version;
+    }
+
+    public String getHeader(@NotNull String name) {
+        return headers.get(name.toLowerCase());
+    }
+
+    @Override public String toString() {
+        return method + " " + path + " " + version;
+    }
+
+    public static HttpRequest parse(BufferedReader reader)
+            throws IOException {
+
+        String requestLine = reader.readLine();
+
+        if (requestLine == null || requestLine.isEmpty())
+            return null;
+
+        String[] parts  = requestLine.split(" ");
+        String method   = parts[0];
+        String path     = parts[1];
+        String version  = parts[2];
+
+        Map<String, String> headers = new HashMap<>();
+        String headerLine;
+        while ((headerLine = reader.readLine()) != null
+                && !headerLine.isEmpty()) {
+
+            String[] headerParts = headerLine.split(":", 2);
+
+            if (headerParts.length == 2) {
+                String key   = headerParts[0].trim().toLowerCase();
+                String value = headerParts[1].trim();
+                headers.put(key, value);
+            }
+        }
+
+        return new HttpRequest(method, path, version, headers);
+    }
+}
