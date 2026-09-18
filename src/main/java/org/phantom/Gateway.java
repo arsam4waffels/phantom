60c50ff12a424b584e729fdc755eff6751131d36
Arsam <arsam4waffels@gmail.com>
2026-09-19 00:26:39 +0330

    fix: small improvements

--- src/main/java/org/phantom/Gateway.java
+++ src/main/java/org/phantom/Gateway.java
@@ -5,14 +5,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Gateway {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         final int PORT = 8080;
-        final ServerSocket serverSocket = new ServerSocket(PORT);
 
         while (true) {
-            Socket socket = serverSocket.accept();
-            Thread thread = new Thread(new ClientHandler(socket));
+
+            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+
+                Socket socket = serverSocket.accept();
+                Thread thread = new Thread(new ClientHandler(socket));
+
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
