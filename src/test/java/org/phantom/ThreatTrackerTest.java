ebf3f5fdb906007816033eeac980729df7b95e12
Arsam <arsam4waffels@gmail.com>
2026-09-19 16:57:27 +0330

    test: ThreatTrackerTest passed

--- src/test/java/org/phantom/ThreatTrackerTest.java
+++ src/test/java/org/phantom/ThreatTrackerTest.java
@@ -1,4 +1,39 @@
 package org.phantom;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ThreatTrackerTest {
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    void newIP_shouldNotBeDangerous() {
+        assertFalse(ThreatTracker.isDangerous("/999.999.999.999"));
+    }
+
+    @Test
+    void afterThreeRecords_shouldBeDangerous() {
+        String testIP = "/test-ip-001";
+
+        ThreatTracker.record(testIP);
+        ThreatTracker.record(testIP);
+        assertFalse(ThreatTracker.isDangerous(testIP));
+
+        ThreatTracker.record(testIP);
+        assertTrue(ThreatTracker.isDangerous(testIP));
+    }
+
+    @Test
+    void dangerousIP_countShouldNotExceedThreshold() {
+        String testIP = "/test-ip-002";
+
+        for (int i = 0; i < 10; i++) {
+            ThreatTracker.record(testIP);
+        }
+
+        assertTrue(ThreatTracker.getCount(testIP) >= 3);
+        assertTrue(ThreatTracker.isDangerous(testIP));
+    }
 }
